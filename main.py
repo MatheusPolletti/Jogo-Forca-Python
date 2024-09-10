9a77fc3c732ec6603ab49c98fb85ffe25f76d0f5
Matheus Cauã Polletti <matheus.c.polletti@gmail.com>
2024-09-09 23:12:46 -0300

    Atualização

--- main.py
+++ main.py
@@ -11,95 +11,103 @@ def qualTipo():
     limparConsole()
 
     print("Modos de jogo: Frutas | Países | Animais | Aleatórios")
-    
-    try:
-        tipo = input("Escolha qual tipo você deseja usar para jogar: ").lower().strip()
-        tipo = unidecode(tipo)
-    
-        print(tipo)
-        sleep(2)
-    except:
-        print('\033[31mErro ao escolher o tipo de jogo.\033[0m')
-        sleep(1)
 
-        qualTipo()
-    print('aqui')
-    if tipo == 'frutas':
-        return 1
-    elif tipo == 'paises':
-        return 2
-    elif tipo == 'animais':
-        return 3
-    elif tipo == 'aleatorios':
-        return 4
-    else:
-        qualTipo()
+    while True:
+        try:
+            tipo = input("Escolha qual tipo você deseja usar para jogar: ").lower().strip()
+            tipo = unidecode(tipo) 
+            sleep(1)
+        except:
+            print('\033[31mErro ao escolher o tipo de jogo.\033[0m')
+            sleep(1)
+        finally:
+            if tipo == 'frutas' or tipo == 'fruta':
+                return 1
+            elif tipo == 'paises' or tipo == 'pais':
+                return 2
+            elif tipo == 'animais' or tipo == 'animal':
+                return 3
+            elif tipo == 'aleatorios' or tipo == 'aleatorio':
+                return 4
 
 
 def palavraAdivinhar():
-    try:
-        opcao = qualTipo()
-        if opcao != int:
-            raise ValueError()
-        print(opcao)
+    while True:
+        try:
+            opcao = qualTipo()
 
-        if opcao == 1:
-            with open('frutas.txt', 'r', encoding='utf-8') as arquivo: 
-                linhas = arquivo.readlines()
-        elif opcao == 2:
-            with open('paises.txt', 'r', encoding='utf-8') as arquivo: 
-                linhas = arquivo.readlines()
-        elif opcao == 3:
-            with open('animais.txt', 'r', encoding='utf-8') as arquivo:
-                linhas = arquivo.readlines()
-        elif opcao == 4:
-            with open('aleatorios.txt', 'r', encoding='utf-8') as arquivo:
-                linhas = arquivo.readlines()
+            if opcao == 1:
+                with open('frutas.txt', 'r', encoding='utf-8') as arquivo: 
+                    linhas = arquivo.readlines()
+            elif opcao == 2:
+                with open('paises.txt', 'r', encoding='utf-8') as arquivo: 
+                    linhas = arquivo.readlines()
+            elif opcao == 3:
+                with open('animais.txt', 'r', encoding='utf-8') as arquivo:
+                    linhas = arquivo.readlines()
+            elif opcao == 4:
+                with open('aleatorios.txt', 'r', encoding='utf-8') as arquivo:
+                    linhas = arquivo.readlines()
 
-        palavra_aleatoria = choice(linhas).strip()
-        return palavra_aleatoria
-
-    except:
-        print('\033[31mErro ao abrir o arquivo.\033[0m')
-        sleep(1)
-        palavraAdivinhar()
+            palavra_aleatoria = choice(linhas).strip()
+            return palavra_aleatoria
+        except:
+            print('\033[31mErro ao abrir o arquivo.\033[0m')
+            sleep(1)
 
 
 def letraJogar():
     print('Digite a letra para adivinhar a palavra: ')
 
-    try:
-        letra = unidecode(input()).strip().lower()
+    while True:
+        try:
+            letra = unidecode(input()).strip().lower()
 
-        if len(letra) > 1 or not letra.isalpha():
-            print('\033[31mDigite algo válido.\033[0m')
+            if len(letra) > 1 or not letra.isalpha():
+                print('\033[31mDigite algo válido.\033[0m')
+                sleep(1)
+            else:
+                sleep(1)
+                limparConsole()
+
+                return letra
+        except:
+            print('\033[31mErro ao escolher a letra.\033[0m')
             sleep(1)
 
-            letraJogar()
-        else:
-            sleep(1)
-            limparConsole()
-
-            return letra
-    except:
-        print('\033[31mErro ao escolher a letra.\033[0m')
-        sleep(1)
-        letraJogar()
 
 def Jogar():
     sleep(1)
     limparConsole()
+
+    letras_jogadas = list()
+    palavra_jogador = ''
+
     try:
         palavra_para_adivinhar = palavraAdivinhar()
-        palavra_jogador = '_' * len(palavra_para_adivinhar)
-        chances = 5
 
-        print(palavra_para_adivinhar)
+        for item in palavra_para_adivinhar.split():
+            palavra_jogador += f'{'_' * len(item)} '
+        palavra_jogador = palavra_jogador.strip()
+
+        chances = 5
         
         while chances != 0:
+            sleep(1)
+            limparConsole()
+
+            print(f'Palavras jogadas: {", ".join(letras_jogadas)}')
             print(palavra_jogador)
 
             letra = letraJogar()
+
+            if letra in letras_jogadas:
+                print('Você já jogou essa letra.')
+                sleep(1)
+                limparConsole()
+                continue
+
+            letras_jogadas.append(letra)
             
             if letra in unidecode(palavra_para_adivinhar.lower()):
                 for posicao, item in enumerate(unidecode(palavra_para_adivinhar.lower())):
@@ -112,10 +120,13 @@ def Jogar():
             if palavra_jogador == unidecode(palavra_para_adivinhar.lower()):
                 print('\033[32mParabéns! Você acertou a palavra!\033[0m')
                 print(palavra_para_adivinhar)
+                break
         
-        if chances == 0:
-            print(f'\033[31mVocê perdeu, a palavra era {palavra_para_adivinhar}.\033[0m')
+            if chances == 0:
+                print(f'\033[31mVocê perdeu, a palavra era {palavra_para_adivinhar}.\033[0m')
+                break
     except:
-        Jogar()
+        print('\033[31mErro ao rodar o jogo.\033[0m')
+
 
 Jogar()
